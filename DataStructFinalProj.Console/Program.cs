e1ff448046a2a9ff4d4c98ae6cfe191a5a7abce5
Tyson Meade <tyson.meade@students.snow.edu>
2025-04-09 14:22:31 -0600

    Inventory check fixed

--- DataStructFinalProj.Console/Program.cs
+++ DataStructFinalProj.Console/Program.cs
@@ -4,7 +4,9 @@ var game = new GameRunner();
 
 game.inventory.DisplayInventory();
 game.PressAnyKeyToContinue();
-game.inventory.ProcessNextItem();
+game.DisplayPlayerStats(game.player);
+game.PressAnyKeyToContinue();
+game.inventory.ProcessNextItem(game.player);
 game.PressAnyKeyToContinue();
 game.inventory.DisplayInventory();
 game.PressAnyKeyToContinue();
